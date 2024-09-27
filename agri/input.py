c4254e32e4554a1833fedad70a568f5fcfa53e15
clickTwice26 <shagatoc@gmail.com>
2024-09-27 21:34:27 +0600

    temporarily turned off phoneNumber validation

--- agri/input.py
+++ agri/input.py
@@ -7,19 +7,19 @@ class accountCreationInfo(BaseModel):
     clientDeviceInfo : str | None = None
     accountCreationIp : str | None = None
 
-    @field_validator("phoneNumber")
-    def validate_phoneNumber(cls, value: str):
-        if not value.isdigit() or len(value) != 11 or not value.startswith("01"):
-            raise ValueError("provide a valid phone number")
-        return value
-    @field_validator("email")
-    def validate_email(cls, value: str):
-        if value is None:
-            return None
-        if not value.count("@") == 1 or not len(value.split("@")) > 1:
-            raise ValueError("provide a valid email address")
-        else:
-            return value
+    # @field_validator("phoneNumber")
+    # def validate_phoneNumber(cls, value: str):
+    #     if not value.isdigit() or len(value) != 11 or not value.startswith("01"):
+    #         raise ValueError("provide a valid phone number")
+    #     return value
+    # @field_validator("email")
+    # def validate_email(cls, value: str):
+    #     if value is None:
+    #         return None
+    #     if not value.count("@") == 1 or not len(value.split("@")) > 1:
+    #         raise ValueError("provide a valid email address")
+    #     else:
+    #         return value
 
 class aiReplyInput(BaseModel):
     prompt : str
